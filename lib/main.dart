e94f8b8993c87b685ea08477efc077a2e1cfefbb
ImadDz-7 <imadbouamrirene97@gmail.com>
2026-04-21 20:34:32 +0000

    Add App widget in lib folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:i_store_app/app.dart';
+import 'package:i_store_app/utils/theme/theme.dart';
 
+/// ------ Entry point of Flutter App ------ ///
 void main() {
+  // Todo: Add Widgets binding
+  // Todo: Init Local Storage
+  // Todo: Await Native Splash
+  // Todo: Initialize Firebase
+  // Todo: Initialize Authentication
+
   runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      
-    );
-  }
-}
