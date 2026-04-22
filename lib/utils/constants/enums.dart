71aa28a870dc74ac5e11372906d038f77364ea19
ImadDz-7 <imadbouamrirene97@gmail.com>
2026-04-22 12:16:37 +0000

    Add enums file

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,17 @@
+enum ProductType { single, variable }
+
+enum TextSizes { small, midium, large }
+
+enum OrderStates { processing, shipped, delivered, pending, cancelled }
+
+enum PaymentMethods {
+  paypal,
+  googlePay,
+  applePay,
+  visa,
+  masterCard,
+  creditCard,
+  payStack,
+  razorPay,
+  paytm
+}
