a811c58a2874522c41afb75c42d2599b54931412
ImadDz-7 <imadbouamrirene97@gmail.com>
2026-04-22 18:31:22 +0000

    Add packages (http, intl, logger, url launcher, get storage)

--- lib/utils/constants/sized.dart
+++ lib/utils/constants/sized.dart
@@ -33,7 +33,7 @@ class ISizes{
   // Default Spacing between sections
   static const double defaultSpace = 24.0;
   static const double spaceBtwItems = 16.0;
-  static const double spaceBtwSections = 52.0;
+  static const double spaceBtwSections = 32.0;
 
   // BorderRadius
   static const double borderRadiusSm = 4.0;
