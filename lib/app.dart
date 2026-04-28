866ba50d977c1190e36f926e62f58c3e65acdc90
ImadDz-7 <imadbouamrirene97@gmail.com>
2026-04-28 20:21:35 +0000

    Add Image & Texts inside Stack widget

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:i_store_app/features/authentication/screens/on_boarding/on_boarding_screen.dart';
 import 'package:i_store_app/utils/theme/theme.dart';
 
 class App extends StatelessWidget {
@@ -6,10 +8,11 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       themeMode: ThemeMode.system,
       theme: IAppTheme.lightTheme,
       darkTheme: IAppTheme.darkTheme,
+      home: OnBoardingScreen(),
     );
   }
 }
--- lib/features/authentication/screens/on_boarding/on_boarding_screen.dart
+++ lib/features/authentication/screens/on_boarding/on_boarding_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:i_store_app/utils/constants/image_strings.dart';
+import 'package:i_store_app/utils/constants/sized.dart';
+import 'package:i_store_app/utils/constants/text_strings.dart';
+import 'package:i_store_app/utils/helpers/helper_functions.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
@@ -9,13 +13,39 @@ class OnBoardingScreen extends StatelessWidget {
       body: Stack(
         children: [
           /// Horizontal Scrollable Page
-          
+          PageView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(ISizes.defaultSpace),
+                child: Column(
+                  children: [
+                    Image(
+                      width: IHelperFunctions.screenWidth() * 0.8,
+                      height: IHelperFunctions.screenHeight() * 0.59,
+                      image: const AssetImage(IImages.onBoardingImage1),
+                    ),
+                    Text(
+                      ITexts.onBoardingTitle1,
+                      style: Theme.of(context).textTheme.headlineMedium,
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: ISizes.spaceBtwItems),
+                    Text(
+                      ITexts.onBoardingSubTitle1,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
           /// Skip Button
-          
+
           /// Dot Navigation Smooth Page Indicator
-          
+
           /// Circular Button
-          
         ],
       ),
     );
