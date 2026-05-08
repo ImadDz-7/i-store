39cb86e59e3e707bebe93a8bd966b3a262450dc8
ImadDz-7 <imadbouamrirene97@gmail.com>
2026-05-08 19:24:41 +0000

    Designing Signup Screen

--- lib/features/authentication/screens/signup/signup_screen.dart
+++ lib/features/authentication/screens/signup/signup_screen.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:i_store_app/utils/constants/sized.dart';
+import 'package:i_store_app/utils/constants/text_strings.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(ISizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Signup Title
+              Text(
+                ITexts.signupTitle,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(height: ISizes.spaceBtwSections),
+
+              /// Form
+              Form(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
