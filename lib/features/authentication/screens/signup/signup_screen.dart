b3e6e8d59e3b37ece5d0eae1aaa0965c9f6c90c0
ImadDz-7 <imadbouamrirene97@gmail.com>
2026-05-10 22:33:11 +0000

    Add (first & last name, username, email, phone number, password) text fields

--- lib/features/authentication/screens/signup/signup_screen.dart
+++ lib/features/authentication/screens/signup/signup_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:i_store_app/utils/constants/sized.dart';
 import 'package:i_store_app/utils/constants/text_strings.dart';
+import 'package:iconsax/iconsax.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
@@ -28,7 +29,87 @@ class SignupScreen extends StatelessWidget {
                     Row(
                       children: [
                         Expanded(
-                          child: TextFormField(),
+                          child: TextFormField(
+                            expands: false,
+                            decoration: const InputDecoration(
+                              labelText: ITexts.firstName,
+                              prefixIcon: Icon(Iconsax.user),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: ISizes.spaceBtwInputFields),
+                        Expanded(
+                          child: TextFormField(
+                            expands: false,
+                            decoration: const InputDecoration(
+                              labelText: ITexts.lastName,
+                              prefixIcon: Icon(Iconsax.user),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: ISizes.spaceBtwInputFields),
+
+                    /// Username
+                    TextFormField(
+                      expands: false,
+                      decoration: const InputDecoration(
+                        labelText: ITexts.username,
+                        prefixIcon: Icon(Iconsax.user_edit),
+                      ),
+                    ),
+                    const SizedBox(height: ISizes.spaceBtwInputFields),
+
+                    /// Email
+                    TextFormField(
+                      expands: false,
+                      decoration: const InputDecoration(
+                        labelText: ITexts.username,
+                        prefixIcon: Icon(Iconsax.direct),
+                      ),
+                    ),
+                    const SizedBox(height: ISizes.spaceBtwInputFields),
+
+                    /// Phone Number
+                    TextFormField(
+                      expands: false,
+                      decoration: const InputDecoration(
+                        labelText: ITexts.username,
+                        prefixIcon: Icon(Iconsax.call),
+                      ),
+                    ),
+                    const SizedBox(height: ISizes.spaceBtwInputFields),
+
+                    /// Password
+                    TextFormField(
+                      expands: false,
+                      decoration: const InputDecoration(
+                        labelText: ITexts.username,
+                        prefixIcon: Icon(Iconsax.password_check),
+                        suffixIcon: Icon(Iconsax.eye_slash),
+                      ),
+                    ),
+                    const SizedBox(height: ISizes.spaceBtwSections),
+
+                    /// Terms & Conditions Checkbox
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 24,
+                          height: 24,
+                          child: Checkbox(
+                            value: true,
+                            onChanged: (value) {},
+                          ),
+                        ),
+                        const SizedBox(width: ISizes.spaceBtwItems),
+                        Text.rich(
+                          TextSpan(
+                            children: [
+                              TextSpan(text: ITexts.iAgreeTo),
+                            ],
+                          ),
                         ),
                       ],
                     ),
