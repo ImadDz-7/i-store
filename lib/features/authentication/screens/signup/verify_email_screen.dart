825ca76feb9e454c4658e8cdd4354d0fe76988e1
ImadDz-7 <imadbouamrirene97@gmail.com>
2026-05-22 12:44:04 +0000

    Add Image & title, subtitle

--- lib/features/authentication/screens/signup/verify_email_screen.dart
+++ lib/features/authentication/screens/signup/verify_email_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:i_store_app/features/authentication/screens/login/login_screen.dart';
+import 'package:i_store_app/utils/constants/image_strings.dart';
 import 'package:i_store_app/utils/constants/sized.dart';
+import 'package:i_store_app/utils/constants/text_strings.dart';
+import 'package:i_store_app/utils/helpers/helper_functions.dart';
 
 class VerifyEmailScreen extends StatelessWidget {
   const VerifyEmailScreen({super.key});
@@ -13,7 +17,7 @@ class VerifyEmailScreen extends StatelessWidget {
         automaticallyImplyLeading: false,
         actions: [
           IconButton(
-            onPressed: () => Get.back(),
+            onPressed: () => Get.offAll(() => const LoginScreen()),
             icon: const Icon(CupertinoIcons.clear),
           ),
         ],
@@ -21,14 +25,39 @@ class VerifyEmailScreen extends StatelessWidget {
       body: SingleChildScrollView(
         // Padding to Give Default Equal Space in all Screens.
         child: Padding(
-          padding: EdgeInsets.all(ISizes.defaultSpace),
+          padding: const EdgeInsets.all(ISizes.defaultSpace),
           child: Column(
             children: [
               /// Image
-              
+              Image(
+                image: const AssetImage(IImages.deliveredEmailIllustration),
+                width: IHelperFunctions.screenWidth() * 0.6,
+              ),
+              const SizedBox(height: ISizes.spaceBtwSections),
+
               /// Title & Subtitle
+              Text(
+                ITexts.confirmEmail,
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: ISizes.spaceBtwItems),
+              Text(
+                'support@contactsupport.com',
+                style: Theme.of(context).textTheme.labelLarge,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: ISizes.spaceBtwItems),
+              Text(
+                ITexts.confirmEmailSubtitle,
+                style: Theme.of(context).textTheme.labelMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: ISizes.spaceBtwSections),
 
               /// Buttons
+              
+
             ],
           ),
         ),
